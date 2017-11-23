814acd7b5e16b749a88ae20ebb9326d7cda7a924
Dom <phandom@hotmail.fr>
2017-11-23 16:14:57 +0100

    partie 2 + commentaire

--- HTTP/exercice1-partie1/listimages.php
+++ HTTP/exercice1-partie1/listimages.php
@@ -1,10 +1,19 @@
-<?php
+//<?php
+//Partie 1
+//echo 'Listing des images du repertoire miniatures <br />';
+//foreach(scandir('../exercice1-partie3/images/') as $image) {
+//    if ($image != "." && $image != "..") {
+//        $f= '/images/'.$image;
+//        echo "<img src='".$f."' style='width: 128px; height: 128px;'/>";
+//    }
+//}
 
-echo 'Listing des images du repertoire miniatures <br />';
-foreach(scandir('../exercice1-partie3/images/') as $image) {
-    if ($image != "." && $image != "..") {
-        $f= '/images/'.$image;
-        echo "<img src='".$f."' style='width: 128px; height: 128px;'/>";
-    }
+
+
+for($i=1;$i<20;$i++){
+    echo "<img src='https://placem.at/people?txt=Image
+".$i."' style='width: 128px; height: 128px;'/>";
 }
+
+
 ?>
---
+++ HTTP/exercice2/listimagesurl.php
@@ -0,0 +1,10 @@
+<?php
+
+for($i=1;$i<20;$i++){
+    echo "<img src='https://placem.at/people?txt=Image
+".$i."' style='width: 128px; height: 128px;'/>";
+}
+
+
+
+?>
