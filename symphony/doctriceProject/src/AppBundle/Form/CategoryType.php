4e8998ca2e9c4b2d651ce20fd3cc3223ce2bc631
dominique <phandom@hotmail.fr>
2017-12-07 11:54:01 +0100

    formulaire exercice 1

--- symphony/doctriceProject/src/AppBundle/Form/CategoryType.php
+++ symphony/doctriceProject/src/AppBundle/Form/CategoryType.php
@@ -1,12 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dominique
- * Date: 07/12/2017
- * Time: 11:08
- */
 
-class CategoryType
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_category';
+    }
+
 
 }
